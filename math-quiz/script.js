fb6e72cdc6156031efbbbe91e6ba7c39e7ac49b4
Steeeve <skipchou@gmail.com>
2022-03-14 00:36:22 +1100

    Added timer and other improvements

--- math-quiz/script.js
+++ math-quiz/script.js
@@ -15,8 +15,45 @@ function applyChange() {
     noOfScore = 0
     noOfQuestions = 0
 
+    timedOut = false
+
+    timerInterval = setInterval(() => {
+        const timeInSeconds = Math.round(timer.getTime() / 1000);
+        if (timeInSeconds <= timeLimit) {
+            document.getElementById('time').innerText = timeLimit - timeInSeconds;
+            // timedOut = false
+        } else {
+            timer.stop()
+            clearInterval(timerInterval)
+            document.getElementById('time').innerText = 0;
+            timedOut = true
+            checkIfContinue()
+        }
+    }, 100)
+
+    timer.reset()
+    timer.start()
+
+    // make element visible
+    document.getElementById('user-answer').style.visibility = "visible";
+    document.getElementById('submitBtn').style.visibility = "visible";
+
+    // move focus to answer box
+    document.getElementById('user-answer').focus();
+    document.getElementById('user-answer').select();
+
+    console.log(timedOut)
     // re-generate question
     checkIfContinue()
+
+    if (selectedLevel == "Easy") {
+        timeLimit = selectedQns * 4
+    } else if (selectedLevel == "Medium") {
+        timeLimit = selectedQns * 5  
+    } else if (selectedLevel == "Hard") {
+        timeLimit = selectedQns * 5 
+    }
+
 }
 
 function generateQ() {
@@ -26,7 +63,7 @@ function generateQ() {
     document.getElementById('user-answer').focus();
     document.getElementById('user-answer').select();
     
-    console.log(selectedLevel)
+    // console.log(selectedLevel)
 
     if (selectedLevel == "Easy") {
         if (['Addition', 'Subtraction'].includes(selectedMode)) {
@@ -44,7 +81,7 @@ function generateQ() {
             rn1 = Math.floor(Math.random() * 8) + 2
             rn2 = Math.floor(Math.random() * 8) + 2     
         }
-    } else {
+    } else if (selectedLevel == "Hard") {
         if (['Addition', 'Subtraction'].includes(selectedMode)) {
             rn1 = Math.floor(Math.random() * 99) + 2
             rn2 = Math.floor(Math.random() * 97) + 4
@@ -87,22 +124,25 @@ function checkIfContinue() {
     // clear user answer
     document.getElementById('user-answer').value = ''
 
-    if (noOfQuestions < selectedQns) {
+    if (noOfQuestions < selectedQns && timedOut == false) {
         generateQ()
 
         // show question number
         document.querySelector('.titles h2').innerHTML = 'Question ' + String(noOfQuestions + 1)
     } else {
 
+        // stop checking timer
+        clearInterval(timerInterval)
+
         // disable answer box and button
         document.getElementById('user-answer').style.visibility = "hidden";
         document.getElementById('submitBtn').style.visibility = "hidden";
 
-        if (noOfScore == noOfQuestions) {
+        if (noOfScore == selectedQns) {
             medal = "🥇 Perfect!"
-        } else if (noOfScore / noOfQuestions >= 0.9) {
+        } else if (noOfScore / selectedQns >= 0.9) {
             medal = "🥈 Excellent!"
-        } else if (noOfScore / noOfQuestions >= 0.85) {
+        } else if (noOfScore / selectedQns >= 0.85) {
             medal = "🥉 Great job!"
         } else {
             medal = "🤦🏻‍♂️ You can do better!"
@@ -134,20 +174,86 @@ function evaluateAns() {
     }
 }
 
+class Timer {
+    constructor () {
+      this.isRunning = false;
+      this.startTime = 0;
+      this.overallTime = 0;
+    }
+  
+    _getTimeElapsedSinceLastStart () {
+      if (!this.startTime) {
+        return 0;
+      }
+    
+      return Date.now() - this.startTime;
+    }
+  
+    start () {
+      if (this.isRunning) {
+        return console.error('Timer is already running');
+      }
+  
+      this.isRunning = true;
+  
+      this.startTime = Date.now();
+    }
+  
+    stop () {
+      if (!this.isRunning) {
+        return console.error('Timer is already stopped');
+      }
+  
+      this.isRunning = false;
+  
+      this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
+    }
+  
+    reset () {
+
+      this.isRunning = false;
+
+      this.overallTime = 0;
+  
+      if (this.isRunning) {
+        this.startTime = Date.now();
+        return;
+      }
+  
+      this.startTime = 0;
+    }
+  
+    getTime () {
+      if (!this.startTime) {
+        return 0;
+      }
+  
+      if (this.isRunning) {
+        return this.overallTime + this._getTimeElapsedSinceLastStart();
+      }
+  
+      return this.overallTime;
+    }
+}
+
+// timer
+
+timeLimit = 0;
+timedOut = false;
+const timer = new Timer();
+
 noOfScore = 0
 noOfQuestions = 0
 getRadioValue()
 
-// make element visible
-document.getElementById('user-answer').style.visibility = "visible";
-document.getElementById('submitBtn').style.visibility = "visible";
+document.querySelector('.question p').innerHTML = "🎟 Press Start"
 
-// move focus to answer box
-document.getElementById('user-answer').focus();
-document.getElementById('user-answer').select();
+// make element hidden
+document.getElementById('user-answer').style.visibility = "hidden";
+document.getElementById('submitBtn').style.visibility = "hidden";
 
-applyChange()
-checkIfContinue()
+// applyChange()
+// checkIfContinue()
 
 // Get the input field
 var userInput = document.getElementById('user-answer');
@@ -155,7 +261,7 @@ var userInput = document.getElementById('user-answer');
 // Execute a function when the user releases a key on the keyboard
 userInput.addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
+    if (event.key === 'Enter') {
       // Cancel the default action, if needed
       event.preventDefault();
       // Trigger the button element with a click
